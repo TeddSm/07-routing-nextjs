b2a95fe1333d1250598415fe8c2e8e9edf7833ad
Mykhailo Drogobetskyi <C:\Users\DrogobetskyiM\AppData\Roaming\The Bat!>
2026-05-05 15:09:31 +0300

    fix name

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -4,7 +4,7 @@ import {
   QueryClient,
 } from "@tanstack/react-query";
 import { fetchNoteById } from "@/lib/api";
-import NoteDetailsClient from "./NoteDetails.client";
+import NoteDetailsClient from "./NotePreview.client";
 
 interface PageProps {
   params: Promise<{ id: string }>;
